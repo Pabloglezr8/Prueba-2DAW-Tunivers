a62c4aef7ebbbc7e1b77f26d360af7117a0c3c75
Pabloglezr8 <pabligoru99@gmail.com>
2023-09-20 10:06:36 +0200

    Añadimos a la Kuquina

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 const generateRandomColor = () => {
-    const color = '#'+Math.floor(Math.random() * 16777215).toString(16);
-    console.log("color hexadecimal genereado: " + color)
-    return color
+    const color = '#'+ Math.floor(Math.random() * 16777215).toString(16);
+    console.log("color hexadecimal genereado: " + color);
+    return color;
 }
 
-chnageColorButton.addEventListener("click", function () {
+const changeBackgroundColor= () => {
     const newColor = generateRandomColor();
     document.body.style.backgroundColor = newColor;
-});
+}
 
 // Agregas un evento al boton changeColorBtn
-const cambiarColorButton = document.getElementById("changeColorBtn");
+const changeColorBtn = document.getElementById("changeColorBtn");
